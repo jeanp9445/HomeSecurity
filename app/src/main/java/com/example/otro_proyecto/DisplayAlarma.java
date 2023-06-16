da61a4aefd5328ff8e599c55ffe8c224a87fe672
Jean Pierre <jeanpierrecapri7@outlook.com>
2023-06-16 13:24:34 -0500

    enviar actualización

---
+++ app/src/main/java/com/example/otro_proyecto/DisplayAlarma.java
@@ -0,0 +1,18 @@
+package com.example.otro_proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class DisplayAlarma extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_alarma);
+
+    }
+}
